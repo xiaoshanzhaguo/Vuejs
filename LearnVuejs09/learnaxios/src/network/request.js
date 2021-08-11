9848505638f91db9710d8971ca9781480fa131ce
coderqian <xiaoshanzhaguo@163.com>
2021-08-11 19:01:08 +0800

    add axios的拦截器

--- LearnVuejs09/learnaxios/src/network/request.js
+++ LearnVuejs09/learnaxios/src/network/request.js
@@ -7,10 +7,40 @@ export function request(config) {
   // instance的类型是AxiosInstance
   const instance = axios.create({
     baseURL: 'https://123.207.32.32:8000',
-    timeout: 5000
+    timeout: 5000,
+    headers: {
+
+    }
   })
 
-  // 发送真正的网络请求
+  // 2. axios的拦截器 interceptors
+  // 2.1 请求拦截的作用
+  instance.interceptors.request.use(config => {// 这里看源码要传入两个参数
+      console.log(config);
+      // 1.比如config里面的信息不符合服务器的要求
+
+      // 2.比如每次发送网络请求时，都希望在界面中显示一个请求的图标
+      // show
+
+      // 3.某些网络请求(比如登录(token)), 必须携带一些特殊的信息
+
+      // 拦截config后，还要返回出去，不然内部没有config这个东西了
+      return config
+    }
+  ,err => {
+      console.log(err);
+    });
+
+  // 2.2 响应拦截
+  instance.interceptors.response.use(res => {
+    // console.log(res);
+    // 只要 res.data
+    return res.data
+  }, err => {
+    console.log(err);
+  });
+
+  // 3. 发送真正的网络请求
   return instance(config) // return 一个promise,本身返回的就是一个promise
 
   // android代码
@@ -79,3 +109,14 @@ export function request(config, success, failure) {
       failure(err)
     })
 }
+
+function test(aaa, bbb) {
+  // aaa('Hello World')
+  bbb('err message')
+}
+
+test(function (res) {
+  console.log(res);
+}, function (err) {
+  console.log(err);
+})
